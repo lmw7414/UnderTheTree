012197b8a0f6bd613e1bc3f092600a26d193d767
Lee Min Woo <lmw7414@naver.com>
2023-09-20 23:07:35 +0900

    Initial Commit

---
+++ src/test/java/com/littledot/UnderTheTree/UnderTheTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littledot.UnderTheTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderTheTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
